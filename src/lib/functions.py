4467fe63ba053a7df706e8dc30d7c44ddbc6ca54
calbertofilho <84130607+calbertofilho@users.noreply.github.com>
2021-06-07 17:10:43 -0300

    Alterações: 07/06/2021

--- src/lib/functions.py
+++ src/lib/functions.py
@@ -1,8 +1,61 @@
 # pylint: disable=no-member
 
+import os
 import sys
 import pygame
+from pygame.constants import (
+    K_LSHIFT, QUIT, KEYDOWN,
+    K_ESCAPE, K_PAUSE,
+    K_LEFT, K_RIGHT, K_UP, K_DOWN, K_SPACE
+)
 
+
+
+def init_game():
+    pygame.init()
+    pygame.display.init()
+    pygame.mixer.pre_init(frequency = 44100, size = 16, channels = 1, buffer = 512)
+    pygame.key.set_repeat()
+def pause_game(state):
+    pass
 def close_game():
+    pygame.display.quit()
+    pygame.mixer.quit()
     pygame.quit()
     sys.exit()
+
+
+
+def create_screen(main_dir, screen_dimension):
+    os.environ['SDL_VIDEO_CENTERED'] = '1'
+    pygame.display.set_mode(screen_dimension)
+    icon = pygame.image.load(os.path.join(main_dir, 'res\images\icons', 'icon.png')).convert_alpha()
+    pygame.display.set_icon(icon)
+    pygame.display.set_caption('Game R-Type Like')
+    return pygame.display.get_surface()
+
+
+
+def handle():
+    for event in pygame.event.get():                                 # Identifica os eventos
+        if event.type == QUIT:                                       # Evento: Fechar a janela
+            close_game()                                             # Chamada da função de fechar
+        if event.type == KEYDOWN:                                    # Evento: Pressionar tecla
+            if event.key == K_ESCAPE:                                # Testa se a tecla é "ESC"
+                close_game()                                         # Chamada da função de fechar
+            if event.key == K_PAUSE:                                 # Testa se a tecla é "PAUSE"
+                pause_game(True)                                     # Chamada da função de pausar
+def controls():
+    commands = pygame.key.get_pressed()                              # Captura as teclas pressionadas
+    if commands[K_LEFT]:                                             # Testa se a tecla é "←"
+        print('Seta para esquerda')
+    if commands[K_RIGHT]:                                            # Testa se a tecla é "→"
+        print('Seta para direita')
+    if commands[K_UP]:                                               # Testa se a tecla é "↑"
+        print('Seta para cima')
+    if commands[K_DOWN]:                                             # Testa se a tecla é "↓"
+        print('Seta para baixo')
+    if commands[K_SPACE]:                                            # Testa se a tecla é "Espaço"
+        print('Espaço')
+    if commands[K_LSHIFT]:                                           # Testa se a tecla é "Shift Esquerdo"
+        print('Shift esquerdo')
--- src/main.py
+++ src/main.py
@@ -4,50 +4,58 @@ Utilizando a biblioteca: PyGame
 
 Criado por: Carlos Alberto Morais Moura Filho
 Versão: 1.0
-Atualizado em: 06/06/2021
+Atualizado em: 07/06/2021
 '''
-
 # pylint: disable=no-member
 # pylint: disable=c-extension-no-member
 # pylint: disable=no-name-in-module
 
-import os
-from lib.functions import ( close_game )
-import pygame
-from pygame.constants import ( QUIT, KEYDOWN, K_ESCAPE )
 
-MAIN_DIR = os.path.normpath(os.path.join(os.path.dirname(__file__), "..")) # Diretorio do jogo
-SCREEN_WIDTH = 1024                                                  # Comprimento da tela
-SCREEN_HEIGHT = 768                                                  # Altura da tela
+
+# Importação das bibliotecas e dependências
+import os
+import sys
+import pygame
+from pygame.locals import *
+PACKAGE_PARENT = '..'
+SCRIPT_DIR = os.path.dirname(__file__)                                # Diretório atual
+MAIN_DIR = os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)) # Diretório do jogo
+sys.path.append(MAIN_DIR)                                             # Adição do diretório do jogo
+from src.lib.functions import (
+    init_game, pause_game, close_game,
+    create_screen,
+    handle, controls
+)
+
+
+
+# Constantes
+SCREEN_WIDTH, SCREEN_HEIGHT = (1366, 768)                            # Comprimento e altura da tela
 SCREEN_DIMENSION = (SCREEN_WIDTH, SCREEN_HEIGHT)                     # Dimensão da tela
-BACKGROUND = (8, 17, 26)                                             # Cor do fundo
-FPS = 30                                                             # Exibição dos frames p/segundo
+BACKGROUND_COLOR = (8, 17, 26)                                       # Cor do fundo
+FPS = 30                                                             # Valor do FRAMERATE
+
+
 
 def main():
     '''Função principal'''
-    os.environ['SDL_VIDEO_CENTERED'] = '1'
-    pygame.init()
-    pygame.mixer.pre_init(frequency = 44100, size = 16, channels = 1, buffer = 512)
-    screen = pygame.display.set_mode(SCREEN_DIMENSION)
-    pygame.Surface.fill(screen, BACKGROUND)
-    icon = pygame.image.load(f'{MAIN_DIR}/res/images/icons/icon.png').convert_alpha()
-    pygame.display.set_icon(icon)
-    pygame.display.set_caption('Game R-Type Like')
-    clock = pygame.time.Clock()
+    init_game()
+    screen = create_screen(MAIN_DIR, SCREEN_DIMENSION)
+    clock = pygame.time.Clock()                                      # Controle do tempo
+
     while True:
-        clock.tick(FPS)
-        for event in pygame.event.get():
-            if event.type == QUIT:
-                close_game()
-            if event.type == KEYDOWN:
-                if event.key == K_ESCAPE:
-                    close_game()
-        pygame.display.update()
+        clock.tick(FPS)                                              # Controle do FRAMERATE do jogo
+        pygame.Surface.fill(screen, BACKGROUND_COLOR)                # Preenche o fundo com uma cor
+        handle()                                                     # Trata os eventos do jogo
+        controls()                                                   # Trata os controles do jogo
+        pygame.display.update()                                      # Atualização de tela
+
+
 
 try:
     if __name__ == "__main__":
         main()
-except (SyntaxError, ValueError, TypeError, ZeroDivisionError) as exc:
-    print(f"Oops! {exc.__class__} occurred.\n{exc.args}")
+except (SyntaxError, ValueError, TypeError, ZeroDivisionError) as exception:
+    print(f"Oops! {exception.__class__} occurred.\n{exception.args}")
 finally:
     close_game()
